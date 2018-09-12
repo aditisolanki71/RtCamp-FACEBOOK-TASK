9e4a46c11803bf98b2ba2d37009940cf1355744c
aditisolanki71 <36895107+aditisolanki71@users.noreply.github.com>
2018-09-12 20:54:31 +0530

    api changed

--- facebook-config.php
+++ facebook-config.php
@@ -7,5 +7,5 @@ define('APP_SECRET','f670f72f5041ee6be0bbbb7b6aea63ec');
 define('REDIRECT_URL','https://rtcamp-task.herokuapp.com/');
 
 //define('REDIRECT_URL','http://localhost/RtCamp/');
-require_once './lib/Facebook/autoload.php.';
+require_once './lib/Facebook/autoload.php';
 ?>
